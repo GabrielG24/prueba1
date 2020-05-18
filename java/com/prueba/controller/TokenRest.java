43730e9292951bed836786a4bfad8b4be7be5524
GabrielG24 <65384490+GabrielG24@users.noreply.github.com>
2020-05-18 07:46:05 -0400

    Update TokenRest.java

--- java/com/prueba/controller/TokenRest.java
+++ java/com/prueba/controller/TokenRest.java
@@ -1,55 +1,4 @@
 package com.prueba.controller;
-/*
-import java.awt.PageAttributes.MediaType;
-import java.sql.Date;
-import java.time.Instant;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.prueba.dao.CourseDAO;
-import com.prueba.entitys.Course;
-
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.impl.TextCodec;
-
-@RestController
-@RequestMapping("token")
-public class TokenRest {
-
- 
-	@PostMapping
-    public ResponseEntity<String> token() {
- 
-      long time = System.currentTimeMillis();
-      String secretKey = "mySecretKey";
- 
-      final String jwt = Jwts.builder()
-    		  .setId("softtekJWT")
-    		  .setSubject("Gabriel")
-    		  .setIssuedAt(new Date(time))
-    		  .setExpiration(new Date(time+900000))
-    		  .claim("email","example@example.com")
-    		  //.signWith(SignatureAlgorithm.HS256, TextCodec.BASE64.encode(secret))
-    		  .signWith(SignatureAlgorithm.HS512,
-						secretKey.getBytes()).compact();
-      return new ResponseEntity<>("Bearer "+jwt, HttpStatus.OK);
-
-   }	
-
-}
-*/
-
 
 import java.util.Date;
 
@@ -86,10 +35,10 @@ public class TokenRest {
 	    		  .setIssuedAt(new Date(time))
 	    		  .setExpiration(new Date(time+900000))
 	    		  .claim("email","example@example.com")
-	    		  /*.claim("authorities",
+	    		  .claim("authorities",
 							grantedAuthorities.stream()
 									.map(GrantedAuthority::getAuthority)
-									.collect(Collectors.toList()))*/
+									.collect(Collectors.toList()))
 	    		  //.signWith(SignatureAlgorithm.HS256, TextCodec.BASE64.encode(secret))
 	    		  .signWith(SignatureAlgorithm.HS512,
 							secretKey.getBytes()).compact();
